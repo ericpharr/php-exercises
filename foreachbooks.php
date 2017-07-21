a32111753adc0687c1940b38e4dceff648d6ba88
Eric Pharr <ericjpharr@gmail.com>
2017-07-21 11:57:18 -0500

    Added book to array and output average pages and year.

--- foreachbooks.php
+++ foreachbooks.php
@@ -26,15 +26,48 @@ $books = array(
 //Construct a loop that iterates through each book and then each book's keys and values. 
 // Have it output the book's title, then list the key value pairs for the data about each book.
 
+$books['Lord of the Rings'] = array(
+        'published' => 1968,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 1178
+    );
+
 foreach($books as $key => $value){
-    if ($value['pages'] < 300){
+    // if ($value['pages'] < 300){
         echo "--------------" . PHP_EOL;
         echo $key . PHP_EOL;
         foreach($value as $key => $value){
             echo "$key: $value" . PHP_EOL;
         }
-    }
+    // }
 }
+
+$totalPages = 0;
+$totalYears = 0;
+$numberBooks = 0;
+foreach($books as $book => $info){
+    $totalPages += $info['pages'];
+    $totalYears += $info['published'];
+    $numberBooks += 1;
+}
+
+echo PHP_EOL . "Average number of pages: " . $totalPages/$numberBooks . PHP_EOL . "Average year published: " . $totalYears/$numberBooks . PHP_EOL;
+
 
 
 // Update your loop to only show books that were written after 1950.
+
+
+
+
+// BONUSES:
+// 1) Update loop to only include books with less than 300 pages
+// 2) Create a new loop that outputs the average page length and year of publication of the books in the books array 
+// 3) Add the following entry to the $books array:
+//     'Lord of the Rings' => array(
+//         'published' => 1968,
+//         'author' => 'J. R. R. Tolkien',
+//         'pages' => 1178
+//     )
+// Construct another loop that iterates through each book as in the exercise step 2 
+// but the loop should only list the first book of a given author in the books array.
