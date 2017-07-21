2316d7df0f92f57904c420b3d8ebb00f5e9104f9
Eric Pharr <ericjpharr@gmail.com>
2017-07-21 12:23:43 -0500

    Added loop which counts to 100, stops at 10.

--- break_continue.php
+++ break_continue.php
@@ -10,3 +10,9 @@ for ($i = 1; $i <= 100; $i++){
 }
 
 // Create another for loop that counts from 1 to 100, but stops after 10 using break.
+for ($i = 1; $i <= 100; $i++){
+    echo $i . PHP_EOL;
+    if ($i == 10){
+        break;
+    }
+} 
